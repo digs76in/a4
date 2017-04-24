a6a25e0910b421c97e6c55b77d1d7f3f9f3dad4b
Digant Shah <digs76in@gmail.com>
2017-04-23 23:01:08 -0400

    commit updates to Databse Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       // $this->call(TasksTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
         $this->call(EmployeesTableSeeder::class);
     }
 }
